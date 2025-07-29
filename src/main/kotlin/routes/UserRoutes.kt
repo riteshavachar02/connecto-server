6c04870289f513b917f3b4e95b4e0376046d96e5
Ritesh Avachar <124338069+riteshavachar02@users.noreply.github.com>
2025-07-29 17:22:06 +0530

    Add userRoutes with POST API for account creation

--- src/main/kotlin/routes/UserRoutes.kt
+++ src/main/kotlin/routes/UserRoutes.kt
@@ -1,6 +1,7 @@
 package com.example.routes
 
 import com.example.controller.user.UserController
+import com.example.data.models.User
 import com.example.data.requests.CreateUserAccount
 import com.example.data.response.BasicApiResponse
 import com.example.util.ApiResponseMessage.FIELDS_BLANK
@@ -14,14 +15,8 @@ import org.koin.ktor.ext.inject
 fun Route.userRoutes() {
     val userController: UserController by application.inject()
 
-    // GET route (just for checking)
-    get("/api/user/create") {
-        println("GET /api/user/create accessed")
-        call.respondText("GET route working", status = HttpStatusCode.OK)
-    }
-
-    // POST route
     post("/api/user/create") {
+
         val request = call.receiveNullable<CreateUserAccount>() ?: run {
             call.respond(
                 HttpStatusCode.BadRequest,
@@ -47,7 +42,19 @@ fun Route.userRoutes() {
             return@post
         }
 
-        // Proceed to create user here if needed
+        userController.createUser(
+            User(
+                email = request.email,
+                username = request.username,
+                password = request.password,
+                profileImageUrl = "",
+                bio = "",
+                skill = listOf(),
+                gitHubUrl = null,
+                instagramUrl = null,
+                linkedInUrl = null
+            )
+        )
         call.respond(
             HttpStatusCode.OK,
             BasicApiResponse(successful = true, message = "User created successfully")
